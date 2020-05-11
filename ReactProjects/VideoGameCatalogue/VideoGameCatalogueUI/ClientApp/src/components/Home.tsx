5f917dc8eda942ec992867ea5e2b3f64af9364ba
JeannineJ <jeanandthen9@live.com>
2020-05-11 13:59:38 -0400

    icon formatting

--- ReactProjects/VideoGameCatalogue/VideoGameCatalogueUI/ClientApp/src/components/Home.tsx
+++ ReactProjects/VideoGameCatalogue/VideoGameCatalogueUI/ClientApp/src/components/Home.tsx
@@ -2,21 +2,27 @@ import * as React from 'react';
 import { connect } from 'react-redux';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faXbox, faSteam } from '@fortawesome/free-brands-svg-icons';
+import IconFormatter from './shared/IconFormatter';
 var ReactBsTable = require('react-bootstrap-table');
 var BootstrapTable = ReactBsTable.BootstrapTable;
 var TableHeaderColumn = ReactBsTable.TableHeaderColumn;
 
-let data: object[] = [{platform: ['XBox']}, {platform: ['Steam']}, {platform: ['Steam', 'XBox']}];
+let data: object[] = [
+    { id: 1, name: 'Game 1', platform: ['XBox'] },
+    { id: 2, name: 'Game 2', platform: ['Steam'] },
+    { id: 3, name: 'Game 3', platform: ['Steam', 'XBox'] },
+    { id: 4, name: 'Game 4', platform: ['XBox', 'Steam'] }
+];
 
 function platformFormatter(cell: any, row: any) {
-    let returnValue: any = null;
+    let returnValue: any = <IconFormatter list={row.platform} type='platform' />;
 
-    if (cell.toUpperCase() == 'XBOX') {
-        returnValue = <FontAwesomeIcon icon={faXbox}/>;
-    }
-    else if (cell.toUpperCase() == 'STEAM') {
-        returnValue = <FontAwesomeIcon icon={faSteam} />;
-    }
+    return returnValue;
+};
+
+function actionsFormatter(cell: any, row: any) {
+    //let returnValue: any = <IconFormatter list={row.platform} type='platform' />;
+    let returnValue: any = <IconFormatter id={row.id} type='actions' />;
 
     return returnValue;
 };
@@ -29,7 +35,7 @@ const Home = () => (
             <TableHeaderColumn isKey dataField='id'>ID</TableHeaderColumn>
             <TableHeaderColumn dataField='name'>Name</TableHeaderColumn>
             <TableHeaderColumn dataField='platform' dataFormat={platformFormatter}>Platform(s)</TableHeaderColumn>
-            <TableHeaderColumn>Actions</TableHeaderColumn>
+            <TableHeaderColumn dataFormat={actionsFormatter}>Actions</TableHeaderColumn>
         </BootstrapTable>
   </div>
 );
---
+++ ReactProjects/VideoGameCatalogue/VideoGameCatalogueUI/ClientApp/src/components/shared/IconFormatter.tsx
@@ -0,0 +1,60 @@
+﻿import React, { Component } from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faXbox, faSteam } from '@fortawesome/free-brands-svg-icons';
+import { faClipboard, faPencilAlt } from '@fortawesome/free-solid-svg-icons';
+
+function IconsFormatter(props: any) {
+    let element: any = null;
+
+    if (props.type.toUpperCase() === 'PLATFORM') {
+        const list = props.list.sort();
+        element = platformIcons(list).map((x: any) => x);
+    }
+
+    else if (props.type.toUpperCase() === 'ACTIONS') {
+        const id = props.id;
+        element = actionIcons(id).map((x: any) => x);
+    }
+
+    return element;
+};
+
+function platformIcons(platformsList: any) {
+    const compList: any = [];
+
+    if (platformsList.length > 0) {
+        platformsList.map((p: any) => {
+            switch (p.toUpperCase()) {
+            case 'XBOX':
+                compList.push(<FontAwesomeIcon icon={faXbox} className="mr-3" title="XBox" />);
+                break;
+            case 'STEAM':
+                compList.push(<FontAwesomeIcon icon={faSteam} className="mr-3" title="Steam"/>);
+                break;
+            default:
+                break;
+            }
+        });
+    };
+
+    return compList;
+};
+
+function actionIcons(id: any) {
+    const compList: any = [];
+
+    compList.push(detailsIcon(id));
+    compList.push(editIcon(id));
+
+    return compList;
+};
+
+function detailsIcon(dataId: any) {
+    return <FontAwesomeIcon icon={faClipboard} className="mr-3" title="Details"/>;
+}
+
+function editIcon(dataId: any) {
+    return <FontAwesomeIcon icon={faPencilAlt} className="mr-3" title="Edit"/>;
+}
+
+export default (IconsFormatter);
