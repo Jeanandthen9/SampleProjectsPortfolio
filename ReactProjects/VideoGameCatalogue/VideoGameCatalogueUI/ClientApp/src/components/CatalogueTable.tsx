8cd12ab961c4a18e5d39482018caf98dc1a38e48
JeannineJ <jeanandthen9@live.com>
2020-05-12 16:42:59 -0400

    added CatalogueTable component, refactored

---
+++ ReactProjects/VideoGameCatalogue/VideoGameCatalogueUI/ClientApp/src/components/CatalogueTable.tsx
@@ -0,0 +1,60 @@
+﻿import * as React from 'react';
+import IconFormatter from './shared/IconFormatter';
+const axios = require('axios').default;
+var ReactBsTable = require('react-bootstrap-table');
+var BootstrapTable = ReactBsTable.BootstrapTable;
+var TableHeaderColumn = ReactBsTable.TableHeaderColumn;
+
+type CatalogueState = {fakeData: object[], realData: object[]}
+
+class CatalogueTable extends React.Component<{}, CatalogueState> {
+    constructor(props: any) {
+        super(props);
+        this.state = {
+            fakeData: [
+                { id: 1, title: 'Game 1', platforms: ['XBox'] },
+                { id: 2, title: 'Game 2', platforms: ['Steam'] },
+                { id: 3, title: 'Game 3', platforms: ['Steam', 'XBox'] },
+                { id: 4, title: 'Game 4', platforms: ['XBox', 'Steam'] }
+            ],
+            realData: []
+        };
+    }
+
+    platformsFormatter(cell: any, row: any) {
+        let returnValue: any = <IconFormatter list={row.platforms} type='platform'/>;
+
+        return returnValue;
+    };
+
+    actionsFormatter(cell: any, row: any) {
+        let returnValue: any = <IconFormatter data={row} type='actions'/>;
+
+        return returnValue;
+    };
+
+    getData() {
+        axios.get('Catalogue/GetCatalogue')
+            .then(function(response: any) {
+                console.log(response.data.data);
+                return response.data.data;
+            }).catch(function(error: any) {
+                console.log(error);
+            });
+    };
+
+    render() {
+        return (
+            <div>
+                <BootstrapTable data={this.state.fakeData} search={true} striped hover>
+                    <TableHeaderColumn isKey dataField='id' dataSort sortIndicator={true}>ID</TableHeaderColumn>
+                    <TableHeaderColumn dataField='title' dataSort sortIndicator={true}>Name</TableHeaderColumn>
+                    <TableHeaderColumn dataField='platforms' dataFormat={this.platformsFormatter}>Platform(s)</TableHeaderColumn>
+                    <TableHeaderColumn dataFormat={this.actionsFormatter}>Actions</TableHeaderColumn>
+                </BootstrapTable>
+            </div>
+        );
+    }
+}
+
+export default (CatalogueTable);
--- ReactProjects/VideoGameCatalogue/VideoGameCatalogueUI/ClientApp/src/components/Home.tsx
+++ ReactProjects/VideoGameCatalogue/VideoGameCatalogueUI/ClientApp/src/components/Home.tsx
@@ -1,39 +1,12 @@
 import * as React from 'react';
 import { connect } from 'react-redux';
-import IconFormatter from './shared/IconFormatter';
-var ReactBsTable = require('react-bootstrap-table');
-var BootstrapTable = ReactBsTable.BootstrapTable;
-var TableHeaderColumn = ReactBsTable.TableHeaderColumn;
-
-let data: object[] = [
-    { id: 1, name: 'Game 1', platforms: ['XBox'] },
-    { id: 2, name: 'Game 2', platforms: ['Steam'] },
-    { id: 3, name: 'Game 3', platforms: ['Steam', 'XBox'] },
-    { id: 4, name: 'Game 4', platforms: ['XBox', 'Steam'] }
-];
-
-function platformsFormatter(cell: any, row: any) {
-    let returnValue: any = <IconFormatter list={row.platforms} type='platform' />;
-
-    return returnValue;
-};
-
-function actionsFormatter(cell: any, row: any) {
-    let returnValue: any = <IconFormatter data={row} type='actions' />;
-
-    return returnValue;
-};
+import CatalogueTable from './CatalogueTable';
 
 const Home = () => (
     <div>
         <h1>Home</h1>
-        <p>Table goes here dummy</p>
-        <BootstrapTable data={data} search={true} striped hover>
-            <TableHeaderColumn isKey dataField='id' dataSort sortIndicator={true}>ID</TableHeaderColumn>
-            <TableHeaderColumn dataField='name' dataSort sortIndicator={true}>Name</TableHeaderColumn>
-            <TableHeaderColumn dataField='platform' dataFormat={platformsFormatter}>Platform(s)</TableHeaderColumn>
-            <TableHeaderColumn dataFormat={actionsFormatter}>Actions</TableHeaderColumn>
-        </BootstrapTable>
+        <p>Main Catalogue goes here dummy</p>
+        <CatalogueTable/>
   </div>
 );
 
