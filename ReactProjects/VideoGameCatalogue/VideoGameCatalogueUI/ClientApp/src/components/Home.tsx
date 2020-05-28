47ed5acb6239edbc01271bf8f638009d6ecf3e5d
JeannineJ <jeanandthen9@live.com>
2020-05-28 09:49:15 -0400

    Revert "added CatalogueTable component, refactored"
    This reverts commit 8cd12ab961c4a18e5d39482018caf98dc1a38e48.

--- ReactProjects/VideoGameCatalogue/VideoGameCatalogueUI/ClientApp/src/components/Home.tsx
+++ ReactProjects/VideoGameCatalogue/VideoGameCatalogueUI/ClientApp/src/components/Home.tsx
@@ -1,12 +1,39 @@
 import * as React from 'react';
 import { connect } from 'react-redux';
-import CatalogueTable from './CatalogueTable';
+import IconFormatter from './shared/IconFormatter';
+var ReactBsTable = require('react-bootstrap-table');
+var BootstrapTable = ReactBsTable.BootstrapTable;
+var TableHeaderColumn = ReactBsTable.TableHeaderColumn;
+
+let data: object[] = [
+    { id: 1, name: 'Game 1', platforms: ['XBox'] },
+    { id: 2, name: 'Game 2', platforms: ['Steam'] },
+    { id: 3, name: 'Game 3', platforms: ['Steam', 'XBox'] },
+    { id: 4, name: 'Game 4', platforms: ['XBox', 'Steam'] }
+];
+
+function platformsFormatter(cell: any, row: any) {
+    let returnValue: any = <IconFormatter list={row.platforms} type='platform' />;
+
+    return returnValue;
+};
+
+function actionsFormatter(cell: any, row: any) {
+    let returnValue: any = <IconFormatter data={row} type='actions' />;
+
+    return returnValue;
+};
 
 const Home = () => (
     <div>
         <h1>Home</h1>
-        <p>Main Catalogue goes here dummy</p>
-        <CatalogueTable/>
+        <p>Table goes here dummy</p>
+        <BootstrapTable data={data} search={true} striped hover>
+            <TableHeaderColumn isKey dataField='id' dataSort sortIndicator={true}>ID</TableHeaderColumn>
+            <TableHeaderColumn dataField='name' dataSort sortIndicator={true}>Name</TableHeaderColumn>
+            <TableHeaderColumn dataField='platform' dataFormat={platformsFormatter}>Platform(s)</TableHeaderColumn>
+            <TableHeaderColumn dataFormat={actionsFormatter}>Actions</TableHeaderColumn>
+        </BootstrapTable>
   </div>
 );
 
