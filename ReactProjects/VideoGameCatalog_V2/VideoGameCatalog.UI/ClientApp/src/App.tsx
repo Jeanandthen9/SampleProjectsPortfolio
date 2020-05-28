8d9fc7b37a9c5452f9cac3c0b6915fcbf2b14b48
JeannineJ <jeanandthen9@live.com>
2020-05-28 11:04:41 -0400

    added fontAwesome icon library to App.tsx

--- ReactProjects/VideoGameCatalog_V2/VideoGameCatalog.UI/ClientApp/src/App.tsx
+++ ReactProjects/VideoGameCatalog_V2/VideoGameCatalog.UI/ClientApp/src/App.tsx
@@ -5,8 +5,14 @@ import Home from './components/Home';
 import Counter from './components/Counter';
 import FetchData from './components/FetchData';
 
+import { library } from '@fortawesome/fontawesome-svg-core';
+import { fab } from '@fortawesome/free-brands-svg-icons';
+import { faClipboard, faPencilAlt, faTrash } from '@fortawesome/free-solid-svg-icons';
+
 import './custom.css'
 import 'react-bootstrap-table-next/dist/react-bootstrap-table2.min.css';
+
+library.add(fab, faClipboard, faPencilAlt, faTrash);
 
 export default () => (
     <Layout>
